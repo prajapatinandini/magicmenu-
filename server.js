591ae28e8c0501dec29d9742c0b2c967b83d16e4
prajapatinandini <nandiniprajapati422@gmail.com>
2025-07-15 12:31:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,36 +1,75 @@
-const express=require('express');
-const app=express();
+const express = require("express");
+const app = express();
+const http = require("http");
 const mongoose = require("mongoose");
 const path = require("path");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+
+const PORT = process.env.PORT || 9000;
+const MONGO_URI = "mongodb+srv://nandini:nandini21@cluster0.eogaihp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  }
+});
+
+
+app.set("io", io);
+
+mongoose.connect(MONGO_URI)
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.use("/uploads", express.static("uploads"));
+app.use(express.static(path.join(__dirname, "public")));
+
+
 const authRoutes = require("./routes/owner");
 const cafeRoutes = require("./routes/cafe");
 const menuRoutes = require("./routes/menu");
 const tableRoutes = require("./routes/table");
 const orderRoutes = require("./routes/order");
 const customerRoutes = require("./routes/customer");
-const PORT=9000;
-
-const uri="mongodb+srv://nandini:nandini21@cluster0.eogaihp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(uri);
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use("/uploads", express.static("uploads"));
-
+const subRoutes = require("./routes/subscription");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/cafe", cafeRoutes);
-app.use("/api/menu",menuRoutes);
-app.use("/api/table",tableRoutes);
-app.use("/api/order",orderRoutes);
-app.use("/api/customer",customerRoutes);
+app.use("/api/menu", menuRoutes);
+app.use("/api/table", tableRoutes);
+app.use("/api/order", orderRoutes);
+app.use("/api/customer", customerRoutes);
+app.use("/api/subcription",subRoutes);
 
 
-//686f744449913bc8aae4bfec
-app.listen(PORT,()=>{
-    console.log(`server running at http://localhost:${PORT}`)
+io.on("connection", (socket) => {
+  console.log(" New client connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log(" Client disconnected:", socket.id);
+  });
 });
+
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
